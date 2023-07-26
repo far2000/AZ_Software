03c8337dc5c3497298a233c243699b6c04404d57
hamedrashidpour <hamedrashid1421@gmail.com>
2023-07-26 13:43:40 +0330

    add wanted information variables

---
+++ personal_information.py
@@ -0,0 +1,15 @@
+def main():
+    name = ""
+    last_name = ""
+    phone_number = ""
+    email_address = ""
+    address = ""
+    gender = ""
+    job = ""
+    degree = ""
+    age = ""
+    appearance = ""
+
+if __name__ == "__main__":
+    main()
+

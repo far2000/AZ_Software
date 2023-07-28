671f8246022a231bf37419dcfc38774ca01d10b6
hamedrashidpour <hamedrashid1421@gmail.com>
2023-07-28 10:05:47 +0330

    get inputs

--- personal_information.py
+++ personal_information.py
@@ -1,15 +1,14 @@
 def main():
-    name = ""
-    last_name = ""
-    phone_number = ""
-    email_address = ""
-    address = ""
-    gender = ""
-    job = ""
-    degree = ""
-    age = ""
+    name = input("please enter your name: ")
+    last_name = input("please enter your last name: ")
+    phone_number = input("please enter your phonr number: ")
+    email_address = input("please enter your email address: ")
+    address = input("please enter your address: ")
+    gender = input("please enter your gender (female\male\others): ")
+    job = input("please enter your job; ")
+    degree = input("please enter your degree (Associate\ Bachelor's\ Graduate\ Professional): ")
+    age = input("please enter your age (integer): ")
     appearance = ""
 
 if __name__ == "__main__":
     main()
-

7ad43d83c934c3b4b27ff7130c4f3eca5432daa0
hamedrashidpour <hamedrashid1421@gmail.com>
2023-07-28 10:56:16 +0330

    add apearance_information function

--- personal_information.py
+++ personal_information.py
@@ -5,7 +5,7 @@ def get_name():
 
 
 def get_last_name():
-    last_name = input ("please enter your lasr name: ")
+    last_name = input ("please enter your last name: ")
     return last_name
 
 def get_email_address():
@@ -60,6 +60,34 @@ def get_phone_number():
     phone_number = input("please enter your phone number: ")
     return phone_number
 
+def appearance_information():
+    hair_colour = input("please enter your hair colour (black\ brown\ yellow\ others): ")
+    if hair_colour == "black":
+        hair_colour = "black"
+    elif hair_colour == "brown":
+        hair_colour = "brown"
+    elif hair_colour == "yellow":
+        hair_colour = "yellow"
+    elif hair_colour == "others":
+        hair_colour = "others"
+    else:
+        hair_colour = "prefer not to say"
+    
+    eye_colour = input ("pleae enter your eye colour (black\ blue\ green\ brown\ others): ")
+    if eye_colour == "black":
+        eye_colour = "black"
+    elif eye_colour == "brown":
+        eye_colour = "brown"
+    elif eye_colour == "green":
+        eye_colour = "green"
+    elif eye_colour == "blue":
+        eye_colour = "blue"
+    elif eye_colour == "others":
+        eye_colour = "others"
+    else:
+        eye_colour = "prefer not to say"
+    
+    return ("hair colour is " + hair_colour,"hair colour is " + eye_colour)
 
 def main():
     name = get_name()
@@ -71,7 +99,7 @@ def main():
     job = get_job()
     degree = degree_check()
     age = get_age()
-    appearance = ""
+    appearance = appearance_information()
 
 if __name__ == "__main__":
     main()

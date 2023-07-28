b1325268de3e19c8ba3223684d5970854a61209f
hamedrashidpour <hamedrashid1421@gmail.com>
2023-07-28 10:43:21 +0330

    add get_email_address function

--- personal_information.py
+++ personal_information.py
@@ -8,12 +8,16 @@ def get_last_name():
     last_name = input ("please enter your lasr name: ")
     return last_name
 
+def get_email_address():
+    email_address = input("please enter your email address: ")
+    return email_address
+
 
 def main():
     name = get_name()
     last_name = get_last_name()
     phone_number = input("please enter your phonr number: ")
-    email_address = input("please enter your email address: ")
+    email_address = get_email_address()
     address = input("please enter your address: ")
     gender = input("please enter your gender (female\male\others): ")
     job = input("please enter your job; ")

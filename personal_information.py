999408cbe256048db0ab01557c91b9c43d048cde
hamedrashidpour <hamedrashid1421@gmail.com>
2023-07-28 10:45:17 +0330

    add gender_check function

--- personal_information.py
+++ personal_information.py
@@ -16,13 +16,26 @@ def get_address():
     address = input ("please enter your address: ")
     return address
 
+def gender_check():
+    gender = input("please enter F for female, M for male and O for others: ")
+    if gender == 'F'or gender == 'f':
+        return "Female"
+    
+    elif gender == 'M'or gender == 'm':
+        return "Male"
+    
+    elif gender == 'O'or gender == 'o':
+        return "Others"
+    else:
+        return None
+
 def main():
     name = get_name()
     last_name = get_last_name()
     phone_number = input("please enter your phonr number: ")
     email_address = get_email_address()
     address = get_address()
-    gender = input("please enter your gender (female\male\others): ")
+    gender = gender_check()
     job = input("please enter your job; ")
     degree = input("please enter your degree (Associate\ Bachelor's\ Graduate\ Professional): ")
     age = input("please enter your age (integer): ")
